60fe0b207d7edff5ac756545374a531026bec63a
Alymdrictels <alymdrictels@gmail.com>
2014-08-15 22:59:16 +0200

    improved the directory structure, added day 6

---
+++ language features/interceptors.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+// also known as overloading, with distinct differences
+
+/*
+	__get($property)
+	__set($property,$value)
+	__isset($property)
+	__unset($property)
+	__call($method,$args_array)
+	__callStatic($method,$args_array)
+
+*/
+
+// this is nifty :D
+class Person{
+// private $cornflakes="1";
+	function __get($property){ 	// magic method which takes non-defined
+								// properties as arguments, and returns their calls
+		return $property;
+	}
+}
+$person=new Person;
+print $person->cornflakes; 	// prints "cornflakes" for private $cornflakes
+							// prints "1" for public $cornflakes
+// can be used to call a method by checking whether it exists
+// then constructing a method call string $this->$nameOfMethod
+							
+ ?>
---
+++ language features/interceptors_contd.php
@@ -0,0 +1,75 @@
+﻿<?php
+
+// also known as overloading, with distinct differences
+
+/*
+	__get($property)
+	__set($property,$value)
+	__isset($property)
+	__unset($property)
+	__call($method,$args_array)
+	__callStatic($method,$args_array)
+
+*/
+
+
+class Person{
+	private $_name;
+	private $_age;
+	function __set($property,$value){ 	// magic method which takes non-defined
+		$property=ucfirst($property);
+		$method="set{$property}";
+		// property and value as arguments, and returns their calls
+		// print $method; // for debug
+		if (method_exists($this,$method)){
+			return $this->$method($value);
+		}
+	}
+	function setName($name){
+		
+		$this->_name=$name;
+		
+		if (!is_null($name)){
+			$this->_name=strtoupper($this->_name); // we check
+			// whether the magic setter did something
+			// by capitalizing the _name var
+		}
+	}
+	function setAge($age){
+		$this->_age=strtoupper($age);
+	}
+	function getName(){
+		return $this->_name;
+	}
+	function __unset($property){
+		print $this ."::". $property ." is attempted to be unset!";
+		
+	
+	}
+	function __toString(){
+		return "Person";
+	}
+	
+}
+$person=new Person;
+$person->name="Marcus"; 	
+//print $person->getName(); // prints "MARCUS"
+
+// just as the name implies, interceptor methods can capture
+// variables as they are defined or set and filter them							
+							
+// i'll admit that these seem VERY situational to me at the moment
+// case in point,
+// the next magic method is __unset, called when unset()
+// is called							
+
+
+
+unset($person->_name); // cannot unset private properties
+
+
+
+
+							
+							
+ ?>
